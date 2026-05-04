c88031f09decb46475d2810951673182400a221e
Praneeth3394 <mandali.praneeth@gmail.com>
2026-05-04 15:29:34 +0530

    Update main.tf

--- Terraform/User/main.tf
+++ Terraform/User/main.tf
@@ -1,1 +1,18 @@
+terraform {
+cloud { 
+    
+    organization = "nagapraneeth" 
 
+    workspaces { 
+      name = "user" 
+    } 
+  } 
+  required_providers {
+    genesyscloud = {
+      source  = "MyPureCloud/genesyscloud"
+      version = "~> 1.70.0"
+    }
+  }
+ }
+
+provider "genesyscloud" {}
