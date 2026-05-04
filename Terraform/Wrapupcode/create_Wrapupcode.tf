4572260219056d4c35c73931e6a4187e52381bd5
Praneeth3394 <mandali.praneeth@gmail.com>
2026-05-04 18:26:33 +0530

    Create create_Wrapupcode.tf

---
+++ Terraform/Wrapupcode/create_Wrapupcode.tf
@@ -0,0 +1,8 @@
+resource "genesyscloud_routing_wrapupcode" "wrapupcode" {
+  name        = "Cx_As_Code_wrapupcode"
+  description = "Cx_As_Code_wrapupcode"
+}
+resource "genesyscloud_routing_wrapupcode" "Cx_As_Code_wrapupcode_sales" {
+  name        = "Cx_As_Code_wrapupcode_sales"
+  description = "Cx_As_Code_wrapupcode_sales"
+}
