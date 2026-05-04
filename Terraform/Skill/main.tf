a90e834f5038e7ba20dcbfabb15ab4a5706f05f0
Praneeth3394 <mandali.praneeth@gmail.com>
2026-05-04 15:29:00 +0530

    Update main.tf

--- Terraform/Skill/main.tf
+++ Terraform/Skill/main.tf
@@ -1,1 +1,18 @@
+terraform {
+cloud { 
+    
+    organization = "nagapraneeth" 
 
+    workspaces { 
+      name = "skill" 
+    } 
+  } 
+  required_providers {
+    genesyscloud = {
+      source  = "MyPureCloud/genesyscloud"
+      version = "~> 1.70.0"
+    }
+  }
+ }
+
+provider "genesyscloud" {}
