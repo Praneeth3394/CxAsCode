20d9052a7b11363467ac694d415b8857974fb144
Praneeth3394 <mandali.praneeth@gmail.com>
2026-05-04 16:40:51 +0530

    Update queue.tf

--- Terraform/Queue/queue.tf
+++ Terraform/Queue/queue.tf
@@ -1,5 +1,5 @@
 resource "genesyscloud_routing_queue" "Queue_1" {
-  name = "Example Queue_CICD_demo_3"
+  name = "Example Queue_CICD_demo_1"
   media_settings_callback {
     enable_auto_answer        = false
     mode                      = "AgentFirst"
