4eb7a44e2e0d64d28f112f4230e871509f4fae31
Praneeth3394 <mandali.praneeth@gmail.com>
2026-05-05 17:10:42 +0530

    Update create_site.tf

--- Terraform/Site/create_site.tf
+++ Terraform/Site/create_site.tf
@@ -1,15 +1,15 @@
 resource "genesyscloud_telephony_providers_edges_site" "example" {
   name        = "Cx_As_Code_Site"
   description = "Cx_As_Code_Site"
-  location_id = genesyscloud_location.location.id
+  location_id = var.location_id
   media_model = "Cloud"
 
   outbound_routes {
-    name                   = "Test outbound route"
+    name                  = "Test outbound route"
     description           = "Test outbound route description"
     classification_types  = ["International"]
-    enabled              = true
-    distribution         = "SEQUENTIAL"
+    enabled               = true
+    distribution           = "SEQUENTIAL"
     external_trunk_base_ids = [
       genesyscloud_telephony_providers_edges_trunkbasesettings.trunk-base.id
     ]
