534dfcd2c9d8c3049cb0bdf32451fae986f789b8
Praneeth3394 <mandali.praneeth@gmail.com>
2026-05-05 15:29:18 +0530

    Create create_DID.tf

---
+++ Terraform/DID/create_DID.tf
@@ -0,0 +1,7 @@
+resource "genesyscloud_telephony_providers_edges_did_pool" "Cx_As_Code_did_pool" {
+  start_phone_number = "+13175550000"
+  end_phone_number   = "+13175550000"
+  description        = "Cx_As_Code_did_pool"
+  comments           = "Cx_As_Code_did_pool"
+  pool_provider      = "PURE_CLOUD"
+}
