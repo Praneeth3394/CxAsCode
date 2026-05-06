5e9fb553373fb964016e4886416eef3b0781efd3
Praneeth3394 <mandali.praneeth@gmail.com>
2026-05-06 15:58:11 +0530

    Update create_site.tf

--- Terraform/Site/create_site.tf
+++ Terraform/Site/create_site.tf
@@ -4,16 +4,3 @@ resource "genesyscloud_telephony_providers_edges_site" "example" {
   location_id = var.location_id
   media_model = "Cloud"
 }
-
-resource "genesyscloud_telephony_providers_edges_site_outbound_route" "example" {
-  site_id              = genesyscloud_telephony_providers_edges_site.example.id
-  name                 = "Test outbound route"
-  description          = "Test outbound route description"
-  classification_types = ["International"]
-  enabled              = true
-  distribution         = "SEQUENTIAL"
-
-  external_trunk_base_ids = [
-    genesyscloud_telephony_providers_edges_trunkbasesettings.trunk-base.id
-  ]
-}
