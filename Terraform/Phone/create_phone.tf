efdb314bb4a1e2ffcd06226c530b84d0926e58fe
Praneeth3394 <mandali.praneeth@gmail.com>
2026-05-11 12:34:06 +0530

    Update create_phone.tf

--- Terraform/Phone/create_phone.tf
+++ Terraform/Phone/create_phone.tf
@@ -3,9 +3,5 @@ resource "genesyscloud_telephony_providers_edges_phone" "Cx_As_Code_phone" {
   state                  = "active"
   site_id                = "L2FwaS92Mi90ZWxlcGhvbnkvcHJvdmlkZXJzL2VkZ2VzL3NpdGVzLzJhNWYyMjdhLTM1ZmItNDlkZS1hMTM5LTA0MGFmZjQ0Zjk2OA"
   phone_base_settings_id = "L2FwaS92Mi90ZWxlcGhvbnkvcHJvdmlkZXJzL2VkZ2VzL3Bob25lYmFzZXNldHRpbmdzL2RhYzU2MDBmLTNkYzAtNGE1OC1iYWViLTY2NGY3NzgxMjc4Mw"
-
-  line_properties {
-    line_address = []
-  }
   web_rtc_user_id = "9e8e32eb-1b9f-478a-aa51-1762a4eb2aea"
 }
