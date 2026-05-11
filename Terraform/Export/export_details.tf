446e1374044efe3c1a71242a4f1a1ffbe022cfe8
Praneeth3394 <mandali.praneeth@gmail.com>
2026-05-11 12:03:19 +0530

    Create export_details.tf

---
+++ Terraform/Export/export_details.tf
@@ -0,0 +1,12 @@
+resource "genesyscloud_tf_export" "export_phone_prerequisites" {
+  directory             = "./generated"
+  export_format         = "hcl"
+  log_permission_errors = true
+  include_state_file    = false
+
+  resource_types = [
+    "genesyscloud_telephony_providers_edges_site",
+    "genesyscloud_telephony_providers_edges_phonebasesettings",
+    "genesyscloud_user"
+  ]
+}
