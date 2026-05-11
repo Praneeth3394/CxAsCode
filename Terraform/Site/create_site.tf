0a637cd0e654d0ffd46fde6ef7bbeb78e51a871d
Praneeth3394 <mandali.praneeth@gmail.com>
2026-05-11 10:52:55 +0530

    Update create_site.tf

--- Terraform/Site/create_site.tf
+++ Terraform/Site/create_site.tf
@@ -4,8 +4,3 @@ resource "genesyscloud_telephony_providers_edges_site" "Cx_As_Code_Site" {
   location_id = "deb6cc31-380f-4c7c-ad52-ccdaaae93537"
   media_model = "Cloud"
 }
-
-output "site_id" {
-  description = "ID of the created Genesys Cloud Site"
-  value       = genesyscloud_telephony_providers_edges_site.Cx_As_Code_Site.id
-}
