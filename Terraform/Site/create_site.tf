3d83d333d97a30eadc7cd7257d935c09a249e191
Praneeth3394 <mandali.praneeth@gmail.com>
2026-05-11 10:58:48 +0530

    Update create_site.tf

--- Terraform/Site/create_site.tf
+++ Terraform/Site/create_site.tf
@@ -1,6 +1,6 @@
-resource "genesyscloud_telephony_providers_edges_site" "Cx_As_Code_Site" {
-  name        = "Cx_As_Code_Site"
-  description = "Cx_As_Code_Site"
-  location_id = "deb6cc31-380f-4c7c-ad52-ccdaaae93537"
+resource "genesyscloud_telephony_providers_edges_site" "Cx_As_Code_Demo_Site" {
+  name        = "Cx_As_Code_Demo_Site"
+  description = "Cx_As_Code_Demo_Site"
+  location_id = "deb6cc31-380f-4c7c-ad52-ccdaaee93537"
   media_model = "Cloud"
 }
