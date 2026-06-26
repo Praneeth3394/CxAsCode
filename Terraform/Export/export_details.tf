1f302aa493f748a9accc39ea842c5579c882a549
Praneeth3394 <mandali.praneeth@gmail.com>
2026-06-26 15:21:05 +0530

    Update export_details.tf

--- Terraform/Export/export_details.tf
+++ Terraform/Export/export_details.tf
@@ -1,6 +1,9 @@
 resource "genesyscloud_tf_export" "test_export" {
-  directory = "./Terraform/Export/generated"
-  export_format = "hcl"
+  directory             = "./Terraform/Export/generated"
+  export_format         = "hcl"
+  log_permission_errors = true
 
-  include_filter_resources = ["*"]
+  include_filter_resources = [
+    "genesyscloud_user"
+  ]
 }
